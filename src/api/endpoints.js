5ac80693e72d13e3056c35c80892e4038b97f6fc
Alex <croitoriu.alexandru.code@gmail.com>
2024-02-14 18:38:34 +0200

    switched endpoints to production

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,31 +1,13 @@
-const endpoints = {
-  company: {
-    setCompany: `http://127.0.0.1:8000/api/organization/set_organization/`, // POST
-    getDepartments: `http://127.0.0.1:8000/api/organization/generate_departments/`, // GET
-  },
-  login: {
-    basic: {
-      login: "http://127.0.0.1:8000/api/token/", 
-      updateToken: "http://127.0.0.1:8000/api/token/refresh/", 
-      setGoogleUserData: "http:///127.0.0.1:8000/api/auth/google/",
-    },
-    google: {
-      getUserInfo: (accessToken) =>
-        `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${accessToken}`,
-    },
-  },
-};
-
-
 // const endpoints = {
-  //   company: {
-    //     setCompany: `https://django-rest-starter-production-e568.up.railway.app/api/organization/set_organization/`, // POST
-    //     getDepartments: "https://django-rest-starter-production-e568.up.railway.app/api/organization/generate_departments/",
+//   company: {
+//     setCompany: `http://127.0.0.1:8000/api/organization/set_organization/`, // POST
+//     getDepartments: `http://127.0.0.1:8000/api/organization/generate_departments/`, // GET
 //   },
 //   login: {
 //     basic: {
-//       login: "https://django-rest-starter-production-e568.up.railway.app/api/token/",
-//       updateToken: "https://django-rest-starter-production-e568.up.railway.app/api/token/refresh/",
+//       login: "http://127.0.0.1:8000/api/token/", 
+//       updateToken: "http://127.0.0.1:8000/api/token/refresh/", 
+//       setGoogleUserData: "http:///127.0.0.1:8000/api/auth/google/",
 //     },
 //     google: {
 //       getUserInfo: (accessToken) =>
@@ -33,5 +15,23 @@ const endpoints = {
 //     },
 //   },
 // };
+
+
+const endpoints = {
+    company: {
+        setCompany: `https://django-rest-starter-production-e568.up.railway.app/api/organization/set_organization/`, // POST
+        getDepartments: "https://django-rest-starter-production-e568.up.railway.app/api/organization/generate_departments/",
+  },
+  login: {
+    basic: {
+      login: "https://django-rest-starter-production-e568.up.railway.app/api/token/",
+      updateToken: "https://django-rest-starter-production-e568.up.railway.app/api/token/refresh/",
+    },
+    google: {
+      getUserInfo: (accessToken) =>
+        `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${accessToken}`,
+    },
+  },
+};
 
 export { endpoints };
